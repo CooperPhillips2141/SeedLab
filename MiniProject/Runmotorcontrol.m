0f8d0c432fd495501f0788d4ecf0154c3acf7827
Noe Avila // eonmakesthings <103462072+eonmakesthings@users.noreply.github.com>
2024-02-26 14:13:31 -0700

    Add files via upload

---
+++ MiniProject/Runmotorcontrol.m
@@ -0,0 +1,34 @@
+%this runs the motor control simulink file given data from your motors
+
+close all;
+
+K=2.2; % DC gain [rad/Vs]
+sigma=10; % time constant reciprocal [1/s]
+Kp = 11;
+open_system('motor_control')
+
+% run the simulation
+
+out=sim('motor_control');
+
+
+%load data from arduino
+load('stepdata.mat')
+
+%graph each plot of voltage and velocity
+figure
+subplot(2,1,1)
+plot(out.Voltage,'--','linewidth',2)
+hold on
+plot(data(:,1),data(:,2),'linewidth',2)
+legend('Simulated','Experimental','location','southeast')
+hold off
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+subplot(2,1,2)
+plot(out.DesiredVelocity,'--','linewidth',2)
+hold on
+plot(out.Velocity,'linewidth',2)
+hold off
+xlabel('Time (s)')
+ylabel('Angular Velocity (rad/s)')
